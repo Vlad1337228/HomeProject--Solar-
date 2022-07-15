d170852303a985aa3f0cbedd6edf00c752e83288
Vlad Kogevnikov <vlad.kogevnikov.com@gmail.com>
2022-07-15 13:49:37 +0400

    first

---
+++ Solar/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-consultation").onmouseover=function(){
+    document.getElementById("btn-consultation").style.background= "#1E90FF";
+}
+
+document.getElementById("btn-consultation").onmouseleave=function(){
+    document.getElementById("btn-consultation").style.background= "none";
+}
